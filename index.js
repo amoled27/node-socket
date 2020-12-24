f7abe2fd934a46d715130a7a296ef51133cdee10
amoled27 <amodshinde1995@gmail.com>
2020-12-24 21:05:09 +0530

    fix to PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const port = process.env.port || 5001;
+const port = process.env.PORT || 5001;
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
